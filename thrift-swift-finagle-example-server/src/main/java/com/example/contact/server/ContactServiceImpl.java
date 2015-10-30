79b21dbbc261db8c03a6d224dfbd862a046ad28a
rojanu <aliustek@gmail.com>
2015-10-30 14:31:24 +0000

    Check for Blank id and correctly return exception within Future

--- thrift-swift-finagle-example-server/src/main/java/com/example/contact/server/ContactServiceImpl.java
+++ thrift-swift-finagle-example-server/src/main/java/com/example/contact/server/ContactServiceImpl.java
@@ -10,6 +10,8 @@ import com.example.contact.server.dao.ContactRepository;
 import com.twitter.util.Future;
 import com.twitter.util.Promise;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -35,10 +37,15 @@ public class ContactServiceImpl implements ContactService {
     }
 
     @Override
-    public Future<Contact> get(String id) throws ContactNotFoundException {
-        final Promise<Contact> promise = new Promise<>();
-        promise.setValue(contactRepository.find(UUID.fromString(id)));
-        return promise;
+    public Future<Contact> get(String id){
+        if(StringUtils.isBlank(id)){
+            return Future.exception(new ContactNotFoundException());
+        }
+        try {
+            return Future.value(contactRepository.find(UUID.fromString(id)));
+        } catch (ContactNotFoundException e) {
+            return Future.exception(e);
+        }
     }
 
     @Override
