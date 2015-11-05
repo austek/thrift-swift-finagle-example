a0541b102bc14a1b15a4e951a60809bd4e8d8c32
austek-mdsol <austek@mdsol.com>
2015-11-05 15:13:25 +0000

    Add support for just in time server

--- thrift-swift-finagle-example-server/src/test/java/com/github/rojanu/contact/server/WhenContactServerUp.java
+++ thrift-swift-finagle-example-server/src/test/java/com/github/rojanu/contact/server/WhenContactServerUp.java
@@ -1,37 +1,49 @@
 package com.github.rojanu.contact.server;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.github.rojanu.client.CloseableClient;
+import com.github.rojanu.config.ConfigValidationException;
 import com.github.rojanu.config.client.ClientConfig;
 import com.github.rojanu.contact.client.ContactClientFactory;
-import com.github.rojanu.server.config.FinagleServerConfig;
+import com.github.rojanu.server.AbstractFinagleServerWithAdminInterface;
+import com.github.rojanu.test.util.server.ConfigurableTest;
+import com.github.rojanu.test.util.server.JustInTimeServer;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
-public class WhenContactServerUp {
+import java.io.IOException;
+
+public class WhenContactServerUp extends ConfigurableTest<ClientConfig> {
     private CloseableClient client;
-    private ContactServer server;
+
+    private JustInTimeServer justInTimeServer;
 
     @BeforeClass
-    public void setUpClass() throws Exception {
-        server = new ContactServer(new FinagleServerConfig());
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    server.start();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+    public void beforeClass() throws IOException, ConfigValidationException {
+        ClientConfig clientConfig = loadConfig(new TypeReference<ClientConfig>() {});
+        if (clientConfig.useJustInTimeServer) {
+            System.out.println("Starting just in time servers");
+            AbstractFinagleServerWithAdminInterface server =
+                    Server$.MODULE$.runEmbedded("classpath:config/development/contact-server.yaml");
+            justInTimeServer = JustInTimeServer.Builder()
+                    .server(server)
+                    .build()
+            ;
+            clientConfig = server.getClientConfig(clientConfig);
+        }
+        client = new ContactClientFactory(clientConfig).getCloseableClient();
     }
 
     @AfterClass
-    public void tearDownClass() throws Exception {
-        server.stop();
+    public void afterClass() throws IOException {
+        if (justInTimeServer != null) {
+            System.out.println("Stopping just in time servers");
+            justInTimeServer.stopSilently();
+        }
+        client.close();
     }
 
     @BeforeMethod
